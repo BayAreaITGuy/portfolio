935e405e209e2f7624ad2f0c797c229baef4515a
BayAreaITGuy <connorreadnour@gmail.com>
2020-09-17 12:53:21 -0700

    Add files via upload
    adding javascript dom button

---
+++ JavaScriptPractice/myFile.js
@@ -0,0 +1,57 @@
+/*thank you Chris Hawkes https://www.youtube.com/watch?v=HnXmI2UVZlU&ab_channel=ChrisHawkes */
+/*element 1 */
+let my_element = document.createElement('h1');
+let my_span = document.createElement('span');
+
+my_span.innerText = "Thank you Chris Hawkes!";
+
+my_element.appendChild(my_span);
+
+my_element.classList.add('custom_class');
+
+document.body.appendChild(my_element);
+
+/*element 2 */
+let my_element2 = document.createElement('h2');
+let my_span2 = document.createElement('span');
+
+my_span2.innerText = "I made this text with pure JavaScript :)";
+
+my_element2.appendChild(my_span2);
+
+my_element2.classList.add('custom_class');
+
+document.body.appendChild(my_element2);
+
+/*element 3 */
+let my_element3 = document.createElement('div');
+let my_p = document.createElement('p');
+
+my_p.innerText = "This sentence uses the second custom class";
+
+my_element3.appendChild(my_p);
+
+my_element3.classList.add('custom_class2');
+
+document.body.appendChild(my_element3);
+
+/*element 4 */
+let my_element4 = document.createElement('div');
+let my_p2 = document.createElement('p');
+
+my_p2.innerText = "This sentence uses the third custom class";
+
+my_element4.appendChild(my_p2);
+
+my_element4.classList.add('custom_class3');
+
+document.body.appendChild(my_element4);
+
+/*Element 5 */
+
+let img = new Image();
+img.src = "https://media.customon.com/unsafe/600x600/img.customon.com//art/2/600/600/b8b5b5/16053/ac6adee35b0f1bee64d2fee4a29209f8.png.jpg";
+
+img.classList.add('custom_class4');
+
+document.body.appendChild(img);
---
+++ JavaScriptPractice/readme.txt
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=HnXmI2UVZlU&ab_channel=ChrisHawkes
