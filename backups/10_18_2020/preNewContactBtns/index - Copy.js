0d8852692ff8d56e68c34271e372f25eb29579ca
BayAreaITGuy <connorreadnour@gmail.com>
2020-10-18 21:26:36 -0700

    Add files via upload

---
+++ backups/10_18_2020/preNewContactBtns/index - Copy.js
@@ -0,0 +1,101 @@
+//Code for the back to ttop button
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+//End //Code for the back to ttop button
+
+//Start typewriter code, taken from https://codepen.io/hi-im-si/pen/DHoup
+var TxtType = function(el, toRotate, period) {
+this.toRotate = toRotate;
+this.el = el;
+this.loopNum = 0;
+this.period = parseInt(period, 10) || 2000;
+this.txt = '';
+this.tick();
+this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+var i = this.loopNum % this.toRotate.length;
+var fullTxt = this.toRotate[i];
+
+if (this.isDeleting) {
+this.txt = fullTxt.substring(0, this.txt.length - 1);
+} else {
+this.txt = fullTxt.substring(0, this.txt.length + 1);
+}
+
+this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+var that = this;
+var delta = 85 - Math.random() * 100; // higher delta = slower typing
+
+/*uncomment to start looping text
+ 
+ if (this.isDeleting) { delta /= 3; } // higher delta = quicker deletion
+
+ if (!this.isDeleting && this.txt === fullTxt) {
+ delta = this.period;
+ this.isDeleting = true;
+ } else if (this.isDeleting && this.txt === '') {
+ this.isDeleting = false;
+ this.loopNum++;
+ delta = 500;
+ }
+*/
+
+setTimeout(function() {
+that.tick();
+}, delta);
+};
+
+window.onload = function() {
+var elements = document.getElementsByClassName('typewrite');
+for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+    new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+}
+// INJECT CSS
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";//text color
+document.body.appendChild(css);
+};
+//End of typewriter
+
+// Scroll to specific values - from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+// scrollTo is the same
+window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+    top: 0, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+// document.querySelector('.hello').scrollIntoView({ 
+//     behavior: 'smooth' 
+// });
+// end of scroll specific values code
